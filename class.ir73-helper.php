f58821ff3f88a4aa4d9cde79a5699aa8d28067d9
rahul_cordiace <rahul.k@cordiace.com>
2021-01-06 13:09:47 +0530

    Formatted IR73_Helpers.php. Mainly the two_level_navigation function. Need to make the whole file WPCS compliant and then the wjole plug-in as well.

--- class.ir73-helper.php
+++ class.ir73-helper.php
@@ -10,162 +10,156 @@
 /**
  * Class IR73
  */
-Class IR73_Helper
-{
-    public function __construct() { }
+class IR73_Helper {
 
-    /**
-     * @param $key
-     * @return string
-     */
-    static function key_to_label( $key ): string
-    {
-        return ucwords( str_replace( ['_', '-'], ' ', $key ) );
-    }
+	public function __construct() { }
 
-    /**
-     * @param $path string
-     * @return string
-     */
-    static function get_assets_url( $path ): string
-    {
-        return get_template_directory_uri() . '/assets/' . $path;
-    }
+	/**
+	 * @param $key
+	 * @return string
+	 */
+	static function key_to_label( $key ): string {
+		return ucwords( str_replace( array( '_', '-' ), ' ', $key ) );
+	}
 
-    /**
-     * @param $path string
-     * @return string
-     */
-    static function get_plugin_assets_url( $path ): string
-    {
-        return IR73_PLUGIN_DIR . 'assets/' . $path;
-    }
+	/**
+	 * @param $path string
+	 * @return string
+	 */
+	static function get_assets_url( $path ): string {
+		return get_template_directory_uri() . '/assets/' . $path;
+	}
 
-    /**
-     * @param $function_name string
-     */
-    static function get_ajax_support( $function_name )
-    {
-        add_action( 'wp_ajax_' . $function_name, $function_name );
-        add_action( 'wp_ajax_nopriv_' . $function_name, $function_name );
-    }
+	/**
+	 * @param $path string
+	 * @return string
+	 */
+	static function get_plugin_assets_url( $path ): string {
+		return IR73_PLUGIN_DIR . 'assets/' . $path;
+	}
 
-    /**
-     * @param $value with newline
-     * @return string
-     */
-    static function first_item_in_multiline_textarea( $value ) :string
-    {
-        return preg_split( '/\r\n|\r|\n/', $value )[0];
-    }
+	/**
+	 * @param $function_name string
+	 */
+	static function get_ajax_support( $function_name ) {
+		add_action( 'wp_ajax_' . $function_name, $function_name );
+		add_action( 'wp_ajax_nopriv_' . $function_name, $function_name );
+	}
 
-    /**
-     * Generate img markup with alt & title
-     * attributes taken from wp_media
-     *
-     * @param int $thumbnail_id
-     * @param array $class_names
-     */
-    static function draw_image( int $thumbnail_id, array $class_names = array(), $attributes = array() )
-    {
-        $image_alt    = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true );
-        $image_title  = get_the_title( $thumbnail_id );
-        $image_url    = wp_get_attachment_image_src( $thumbnail_id, 'full' );
-        ?>
-        <img src="<?php echo $image_url[0] ?>"
-            alt="<?php echo $image_alt ?>"
-            title="<?php echo $image_title ?>"
-            class="<?php echo !empty( $class_names ) ? implode( ' ', $class_names ) : '' ?>"
-            <?php
-            if ( ! empty( $attributes ) ):
-                foreach ( $attributes as $attribute => $value ):
-                    echo $attribute . '="' . $value . '"';
-                endforeach;
-            endif;
-            ?>>
-        <?php
-    }
+	/**
+	 * @param $value with newline
+	 * @return string
+	 */
+	static function first_item_in_multiline_textarea( $value ) :string {
+		return preg_split( '/\r\n|\r|\n/', $value )[0];
+	}
 
-    /**
-     * NOTICE : THIS WILL PREPARE ONLY MENUS WITH 2 LEVELS.
-     * MODIFY THIS TO CREATE MULTI LEVEL MENUS.
-     */
-    static function prepare_two_level_menu( $menu_name )
-    {
-        $menu_items = wp_get_nav_menu_items( $menu_name );
+	/**
+	 * Generate img markup with alt & title
+	 * attributes taken from wp_media
+	 *
+	 * @param int   $thumbnail_id
+	 * @param array $class_names
+	 */
+	static function draw_image( int $thumbnail_id, array $class_names = array(), $attributes = array() ) {
+		$image_alt   = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true );
+		$image_title = get_the_title( $thumbnail_id );
+		$image_url   = wp_get_attachment_image_src( $thumbnail_id, 'full' );
+		?>
+		<img src="<?php echo $image_url[0]; ?>"
+			alt="<?php echo $image_alt; ?>"
+			title="<?php echo $image_title; ?>"
+			class="<?php echo ! empty( $class_names ) ? implode( ' ', $class_names ) : ''; ?>"
+			<?php
+			if ( ! empty( $attributes ) ) :
+				foreach ( $attributes as $attribute => $value ) :
+					echo $attribute . '="' . $value . '"';
+				endforeach;
+			endif;
+			?>
+			>
+		<?php
+	}
 
-        if ( false === $menu_items ) { return false; }
+	/**
+	 * NOTICE : THIS WILL PREPARE ONLY MENUS WITH 2 LEVELS.
+	 * MODIFY THIS TO CREATE MULTI LEVEL MENUS.
+	 *
+	 * @param string $menu_name Name of the menu to retrieve.
+	 * @return array
+	 */
+	public static function prepare_two_level_menu( $menu_name ): array {
 
-        $prepared = [];
+		$menu_items = wp_get_nav_menu_items( $menu_name );
 
-        foreach ( $menu_items as $submenu ) {
-            if ( $submenu->menu_item_parent == 0 ) {
-                $prepared[$submenu->ID] = ['object' => $submenu];
-            } else {
-                if ( empty( $menu[$submenu->menu_item_parent]['has_children'] ) ) {
-                    $prepared[$submenu->menu_item_parent]['has_children'] = true;
-                }
-                $prepared[$submenu->menu_item_parent]['children'][] = $submenu;
-            }
-        }
+		if ( false === $menu_items ) {
+			return false; }
 
-        return $prepared;
-    }
+		$prepared = array();
 
-    static function error_flash( string $text_domain, string $message )
-    {
-        echo _e( $message, $text_domain );
-    }
+		foreach ( $menu_items as $submenu ) {
+			if ( 0 === $submenu->menu_item_parent ) {
+				$prepared[ $submenu->ID ] = array( 'object' => $submenu );
+			} else {
+				if ( empty( $menu[ $submenu->menu_item_parent ]['has_children'] ) ) {
+					$prepared[ $submenu->menu_item_parent ]['has_children'] = true;
+				}
+				$prepared[ $submenu->menu_item_parent ]['children'][] = $submenu;
+			}
+		}
 
-    static function generate_unique_username( string $username )
-    {
-        $username = sanitize_title( $username );
+		return $prepared;
+	}
 
-        static $i;
-        if ( null === $i ) {
-            $i = 1;
-        } else {
-            $i++;
-        }
+	public static function error_flash( string $text_domain, string $message ) {
+		echo _e( $message, $text_domain );
+	}
 
-        if ( ! username_exists( $username ) ) {
-            return $username;
-        }
+	public static function generate_unique_username( string $username ) {
+		$username = sanitize_title( $username );
 
-        $new_username = sprintf( '%s-%s', $username, $i );
+		static $i;
+		if ( null === $i ) {
+			$i = 1;
+		} else {
+			$i++;
+		}
 
-        if ( ! username_exists( $new_username ) ) {
-            return $new_username;
-        } else {
-            return call_user_func( __FUNCTION__, $username );
-        }
-    }
+		if ( ! username_exists( $username ) ) {
+			return $username;
+		}
 
-    static function site_url_without_protocols()
-    {
-        $protocols = array( 'https://', 'https://www.', 'www.' );
-        return str_replace( $protocols, '', get_bloginfo( 'wpurl' ) );
-    }
+		$new_username = sprintf( '%s-%s', $username, $i );
 
-    static function pretty_dump( ...$objects )
-    {
-        foreach ( $objects as $key => $object ) {
-            echo "<pre>", var_dump( $object ), "</pre>";
-        }
-    }
+		if ( ! username_exists( $new_username ) ) {
+			return $new_username;
+		} else {
+			return call_user_func( __FUNCTION__, $username );
+		}
+	}
 
-    /**
-     * Get Custom Created Meta Box Values - Mainly Repeters
-     *
-     * @param integer $post_ID
-     * @param string $key
-     * @param boolean $single
-     * @return array
-     */
-    static function get_meta( int $post_ID, string $key, bool $single = true )
-    {
-        return unserialize( get_post_meta( $post_ID, $key, $single ) );
-    }
+	public static function site_url_without_protocols() {
+		$protocols = array( 'https://', 'https://www.', 'www.' );
+		return str_replace( $protocols, '', get_bloginfo( 'wpurl' ) );
+	}
+
+	public static function pretty_dump( ...$objects ) {
+		foreach ( $objects as $key => $object ) {
+			echo '<pre>', var_dump( $object ), '</pre>';
+		}
+	}
+
+	/**
+	 * Get Custom Created Meta Box Values - Mainly Repeters
+	 *
+	 * @param integer $post_ID
+	 * @param string  $key
+	 * @param boolean $single
+	 * @return array
+	 */
+	public static function get_meta( int $post_ID, string $key, bool $single = true ) {
+		return unserialize( get_post_meta( $post_ID, $key, $single ) );
+	}
 
 }
 
